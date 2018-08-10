09ecbfcc2f5c780df18bfc130fdc5d2355e03a5b
Shweta Rajiv Tripathi <Shweta.Tripathi@igglobal.com>
2018-08-10 18:31:43 +0530

    Api Signin/up Integrated

--- app/src/main/java/com/example/shwetatripathi/mmt/LogIn.java
+++ app/src/main/java/com/example/shwetatripathi/mmt/LogIn.java
@@ -4,12 +4,21 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -19,29 +28,47 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LogIn extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener {
 
-    private Button bsignup;
+    private Button bsignup,email_sign_in_button;
     //private Button SignOut;
     private SignInButton SignIn;
-    private TextView Name,Email;
+    private TextView password,Email;
+    Intent intent;
+    String emailvalidate;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+    String passwordvalidate;
     private GoogleApiClient googleApiClient;
     private static final int REQ_CODE=9001;
+    private static final String REGISTER_URL = "http://54.242.83.222:8080/MakeMyTrip/mmt/authentication/login";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        Email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
         bsignup=findViewById(R.id.button_signup);
-
         SignIn=findViewById(R.id.button_google_login);
+        email_sign_in_button=findViewById(R.id.email_sign_in_button);
+
         SignIn.setOnClickListener(this);
+
+
         GoogleSignInOptions signInOptions=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient=new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
 
-
-
+        //Register Button
         bsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,8 +78,82 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener,Goo
             }
         });
 
+        //Validation
+        emailvalidate = Email.getText().toString();
+        passwordvalidate = password.getText().toString();
 
-    }
+       /* if (Email.getText().toString().length() == 0)//e-mail not empty
+            Email.setError("email id required");
+        else if (!emailvalidate.matches(emailPattern))//invalid email
+            Email.setError("invalid email");
+        else if (password.getText().toString().length() == 0)//password not empty
+            password.setError("password required");
+        else if (!passwordvalidate.matches(PASSWORD_PATTERN))
+            password.setError("password must require 1 special character,1 number,1 capital letter,and must contain letter more than or equal to 4");
+           */
+        email_sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //post request
+
+
+                Map<String, String> params = new HashMap();
+                params.put("username", Email.getText().toString());
+                params.put("password", password.getText().toString());
+
+
+                JSONObject parameters = new JSONObject(params);
+
+
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, REGISTER_URL, parameters, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(LogIn.this, "Success", Toast.LENGTH_LONG).show();
+                    }
+
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LogIn.this, "shweta error", Toast.LENGTH_LONG).show();
+                        intent = new Intent(LogIn.this, SignUp.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }) {
+
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("Content-Type", "application/json; charset=utf-8");
+                        return headers;
+                    }
+
+                };
+
+                // Adding request to request queue
+
+                Volley.newRequestQueue(LogIn.this).add(jsonRequest);
+                Log.d("Json result","value--"+parameters.toString());
+                //  Log.d("Json result","Request---"+jsonObjReq.toString());
+
+                intent = new Intent(LogIn.this, Offers.class);
+                startActivity(intent);
+                finish();
+
+
+        }
+
+                    });
+
+
+
+
+
+
+        }
+
 
     @Override
     public void onClick(View v) {
--- app/src/main/java/com/example/shwetatripathi/mmt/SignUp.java
+++ app/src/main/java/com/example/shwetatripathi/mmt/SignUp.java
@@ -1,13 +1,233 @@
 package com.example.shwetatripathi.mmt;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
+
+    private static final String REGISTER_URL = "http://54.242.83.222:8080/MakeMyTrip/mmt/authentication/signup";
+    String emailvalidate;
+    Intent intent;
+    Button button_SignUp;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+    String passwordvalidate;
+    private TextView fn, ln, Email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        fn = findViewById(R.id.fn);
+        ln = findViewById(R.id.ln);
+        Email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        button_SignUp = findViewById(R.id.button_SignUp);
+
+        //Validation
+        // fn= (TextView) fn.getText();
+        //ln= (TextView) ln.getText();
+
+
+        emailvalidate = Email.getText().toString();
+        passwordvalidate = password.getText().toString();
+
+
+      /*  StringRequest request = new StringRequest(REGISTER_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("CODE", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(SignUp.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+*/
+
+
+
+
+
+
+
+
+       /* if (fn.getText().toString().length() == 0)//First Name not empty
+            fn.setError("First Name required");
+        if (ln.getText().toString().length() == 0)//Last Name not empty
+            ln.setError("Last Name required");
+        if (Email.getText().toString().length() == 0)//e-mail not empty
+            Email.setError("Email Id required");
+        else if (!emailvalidate.matches(emailPattern))//invalid email
+            Email.setError("Invalid email");
+        else if (password.getText().toString().length() == 0)//password not empty
+            password.setError("Password required");
+        else if (!passwordvalidate.matches(PASSWORD_PATTERN))
+            password.setError("Password must require 1 special character,1 number,1 capital letter,and must contain letter more than or equal to 4");
+*/
+
+        button_SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //post request
+
+
+                Map<String, String> params = new HashMap();
+                params.put("fName", fn.getText().toString());
+                params.put("lName", ln.getText().toString());
+                params.put("username", Email.getText().toString());
+                params.put("password", password.getText().toString());
+
+
+                JSONObject parameters = new JSONObject(params);
+/*
+                JSONObject js = new JSONObject();
+                try {
+                   // js.put("name", "anything");
+                    js.put("fName", "qw");
+                    js.put("lName", "qwer");
+                    js.put("username", "qwe@infogain.com");
+                    js.put("password", "zxcvbnmas");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }*/
+
+                // Make request for JSONObject
+                /*JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST, REGISTER_URL, js,
+                        new Response.Listener<JSONObject>() {*/
+
+                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, REGISTER_URL, parameters, new Response.Listener<JSONObject>() {
+
+                    @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(SignUp.this, "Success", Toast.LENGTH_LONG).show();
+                            }
+
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(SignUp.this, "shweta error", Toast.LENGTH_LONG).show();
+                        intent = new Intent(SignUp.this, SignUp.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }) {
+
+                    /*@Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("fName", fn.toString());
+                        params.put("lName", ln.toString());
+                        params.put("username", Email.toString());
+                        params.put("password", password.toString());
+                        //  params.put(KEY_MERCHANTCONFIRMPASSWORD, merchantConfirmPassword);
+                        return params;
+                    }*/
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("Content-Type", "application/json; charset=utf-8");
+                        return headers;
+                    }
+
+                };
+
+                // Adding request to request queue
+
+                Volley.newRequestQueue(SignUp.this).add(jsonRequest);
+                Log.d("Json result","value--"+parameters.toString());
+              //  Log.d("Json result","Request---"+jsonObjReq.toString());
+
+                intent = new Intent(SignUp.this, LogIn.class);
+                startActivity(intent);
+                finish();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(SignUp.this, response, Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(SignUp.this, "shweta error", Toast.LENGTH_LONG).show();
+                                intent = new Intent(SignUp.this, SignUp.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("fName", fn.toString());
+                        params.put("lName", ln.toString());
+                        params.put("username", Email.toString());
+                        params.put("password", password.toString());
+                        //  params.put(KEY_MERCHANTCONFIRMPASSWORD, merchantConfirmPassword);
+                        return params;
+                    }
+                   // @Override
+                    public Map<String,String> getHeader() {
+                        HashMap<String,String> headers = new HashMap();
+                        headers.put("Content-Type","application/json;charset=UTF-8");
+                        return headers;
+
+                    }
+                };
+                 RequestQueue requestQueue = Volley.newRequestQueue(SignUp.this);
+                requestQueue.add(stringRequest);
+                intent=new Intent(SignUp.this,LogIn.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });*/
+
+
+        });
     }
 }
+
